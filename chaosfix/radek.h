de36198f67bbcf24e22085131b8604ed180b51ab
chrissamuel <chrissamuel@e238c9d3-d21e-0410-8078-9a6c68b93a49>
2006-10-07 13:51:01 +0000

    Removed ChaosFix FidoNet BBS Software

--- chaosfix/radek.h
+++
@@ -1,9 +0,0 @@
-struct radek
-{
- char str[128];
- struct radek *next;
-};
-void killstrednik(char *buf,int i,int line);
-void AddLine(struct radek **rad, char *str);
-int LoadList(char *dir_frq, struct radek **ptr,char firstwordonly);
-void memerr(void);
